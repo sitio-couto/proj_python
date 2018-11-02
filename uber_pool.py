d59fc6ce7457694fdae86318d01c83166303bc77
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-11-02 09:02:36 -0300

    implemented input read and floydmarshall

--- uber_pool.py
+++ uber_pool.py
@@ -1,27 +1,55 @@
 from sys import stdin
 from numpy import matrix, inf, ones
 
+class return_decorator():
+    def __init__(self, f):
+        self.f = f
+        self.calls = "\n"
+    def __call__(self, *args):
+        ret = self.f(*args)
+        self.calls += "Function: "+self.f.__name__+"\nReturn:\n"
+        for r in enumerate(ret): self.calls += str(r[1])+"\n"
+        return ret
+
+@return_decorator
 def build_matrix(data):
-    dim = max([max(d[:-1]) for d in data]) + 1
-    wmtx = matrix(ones((dim,dim))*inf)
-    for i in range(dim): wmtx[i,i] = 0
-    for d in data: wmtx[d[0],d[1]] = d[2]
-    return wmtx
+    dim = max([max(d[:-1]) for d in data]) + 1 #Get matrix dimension
+    pmtx = matrix(ones((dim,dim))*inf)         #Initialize parents with inf
+    wmtx = matrix(ones((dim,dim))*inf)         #Initialize weights with inf
+    for i in range(dim):
+        pmtx[i,i] = i                          #Set parent to self
+        wmtx[i,i] = 0                          #Set diagonal to 0
 
-def floyd_warshall(wmtx):
-    print(wmtx)
 
+    for d in data:                             #Adds weights and parents from data
+        wmtx[d[0],d[1]] = d[2]
+        pmtx[d[0],d[1]] = d[0]
+
+    return wmtx, pmtx, dim                     #Returns weigths, parents and dimensions
+
+@return_decorator
+def floyd_warshall(w, p, dim):
+    for k in range(dim):
+        for i in range(dim):
+            for j in range(dim):
+                if (w[i,k]+w[k,j] < w[i,j]):
+                    w[i,j] = w[i,k]+w[k,j]
+                    p[i,j] = p[k,j]
+
+    return w, p
+
+@return_decorator
 def read_input( ):
     weight_data = []   # Vertices and weights
-    pool_data = []
+    pool_data = []     # Passengers path data
 
     line = stdin.readline()[:-1].split(' ') # Processed line
-    while (line != ['']):
+    while (line != ['']):                   # Read first chunk of text (weights)
         weight_data.append([int(line[0]),int(line[1]),float(line[2])])
         line = stdin.readline()[:-1].split(' ')
 
     line = stdin.readline()[:-1].split(' ')
-    while (line != ['']):
+    while (line != ['']):                   # Read second chunck of text (paths)
         if (len(line) == 3): pool_data.append([int(x) for x in line])
         else: pool_data.append([int(line[0]),-1,int(line[1])])
         line = stdin.readline()[:-1].split(' ')
@@ -30,11 +58,11 @@ def read_input( ):
 
 # MAIN #########################################################################
 
-wd, pd = read_input()
+weights, paths = read_input()
+print(read_input.calls)
 
-print(wd)
-print(pd)
+weights, parents, dim = build_matrix(weights)
+print(build_matrix.calls)
 
-wd = floyd_warshall(build_matrix(wd))
-
-print(wd)
+weights, parents = floyd_warshall(weights, parents, dim)
+print(floyd_warshall.calls)
